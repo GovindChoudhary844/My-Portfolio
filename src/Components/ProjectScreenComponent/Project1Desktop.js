4c5bf0816520c8b3648fee575423743900058a2a
Govind Choudhary <govindchoudhary844@gmail.com>
2024-03-28 18:19:18 +0530

    Changes

--- src/Components/ProjectScreenComponent/Project1Desktop.js
+++ src/Components/ProjectScreenComponent/Project1Desktop.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Row, Col, Modal, Button } from "react-bootstrap";
 import "../../App.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function Project1Desktop() {
+  const sliderRef = useRef(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
   const settings = {
     dots: false,
     infinite: true,
@@ -19,43 +23,67 @@ function Project1Desktop() {
 
   // Array of image URLs
   const imageUrls = [
-    "Images/Projects/Project1-Desktop-SS/1.png",
-    "Images/Projects/Project1-Desktop-SS/2.png",
-    "Images/Projects/Project1-Desktop-SS/3.png",
-    "Images/Projects/Project1-Desktop-SS/4.png",
-    "Images/Projects/Project1-Desktop-SS/5.png",
-    "Images/Projects/Project1-Desktop-SS/6.png",
-    "Images/Projects/Project1-Desktop-SS/7.png",
-    "Images/Projects/Project1-Desktop-SS/8.png",
-    "Images/Projects/Project1-Desktop-SS/9.png",
-    "Images/Projects/Project1-Desktop-SS/10.png",
-    "Images/Projects/Project1-Desktop-SS/11.png",
-    "Images/Projects/Project1-Desktop-SS/12.png",
-    "Images/Projects/Project1-Desktop-SS/13.png",
-    "Images/Projects/Project1-Desktop-SS/14.png",
-    "Images/Projects/Project1-Desktop-SS/15.png",
-    "Images/Projects/Project1-Desktop-SS/16.png",
-    "Images/Projects/Project1-Desktop-SS/17.png",
-    "Images/Projects/Project1-Desktop-SS/18.png",
-    "Images/Projects/Project1-Desktop-SS/19.png",
-    "Images/Projects/Project1-Desktop-SS/20.png",
-    "Images/Projects/Project1-Desktop-SS/21.png",
-    "Images/Projects/Project1-Desktop-SS/22.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/1.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/2.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/3.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/4.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/5.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/6.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/7.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/8.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/9.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/10.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/11.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/12.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/13.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/14.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/15.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/16.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/17.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/18.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/19.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/20.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/21.png",
+    "Images/Projects/Project 1/Project1-Desktop-SS/22.png",
   ];
+
+  const handleImageClick = (index) => {
+    setSelectedImageIndex(index);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   return (
     <>
+      <style>
+        {`
+          .Desktop-bg {
+            background-image: url('Images/Projects/Project 1/Desktop.jpg');
+            background-size: cover; 
+            background-position: center;
+          }
+          
+          .slick-prev:before,
+          .slick-next:before {
+            color: black !important; 
+          }
+        `}
+      </style>
       <Row>
         <Col>
           <h1 className="resp-h2">Desktop Mode</h1>
         </Col>
       </Row>
       <Row className="mt-3">
-        <Col sm={12} md={12} lg={12} xl={6} xxl={6}>
+        <Col sm={12} md={12} lg={12} xl={6} xxl={6} className="Desktop-bg">
           <video controls width="100%" height="auto" className="rounded-3">
             <source
-              src="Images/Projects/Project1-Desktop.mp4"
+              src="Images/Projects/Project 1/Project1-Desktop.mp4"
               type="video/mp4"
+              loading="lazy"
             />
           </video>
         </Col>
@@ -82,12 +110,51 @@ function Project1Desktop() {
           <Slider {...settings}>
             {imageUrls.map((imageUrl, index) => (
               <div key={index} className="p-1">
-                <img src={imageUrl} alt="" className="img-fluid" />
+                <img
+                  src={imageUrl}
+                  alt=""
+                  className="img-fluid"
+                  onClick={() => handleImageClick(index)}
+                  style={{ cursor: "pointer" }}
+                  loading="lazy"
+                />
               </div>
             ))}
           </Slider>
         </Col>
       </Row>
+
+      <Modal show={showModal} onHide={closeModal} centered>
+        <Modal.Body>
+          <div className=" d-flex justify-content-between align-items-center">
+            <i
+              className="fa-solid fa-circle-chevron-left position-absolute"
+              onClick={() =>
+                setSelectedImageIndex((prevIndex) =>
+                  prevIndex === 0 ? imageUrls.length - 1 : prevIndex - 1
+                )
+              }
+              style={{ cursor: "pointer" }}
+            ></i>
+            <img
+              src={imageUrls[selectedImageIndex]}
+              alt=""
+              className="img-fluid"
+              style={{ maxHeight: "70vh", objectFit: "contain" }}
+              loading="lazy"
+            />
+            <i
+              className="fa-solid fa-circle-chevron-right"
+              onClick={() =>
+                setSelectedImageIndex((prevIndex) =>
+                  prevIndex === imageUrls.length - 1 ? 0 : prevIndex + 1
+                )
+              }
+              style={{ cursor: "pointer", marginLeft: "-10px" }}
+            ></i>
+          </div>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
